0029c3355260c39bec8c18a91de1a86c6d15bd9a
Jean Max Mezalon <+>
2021-09-29 15:39:45 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ruth Pierre",
+    streetAddress: "1463 E 96th St Brooklyn NY 11236"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key] : value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj}
+
+    delete newObj[key]
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
